7b93a15cb3d1b09c34e4df2f153d795400fa0e1e
Hiten1708 <68720970+Hiten1708@users.noreply.github.com>
2022-06-24 23:04:27 -0400

    personalize website

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -8,10 +8,8 @@ function AboutCard() {
       <Card.Body>
         <blockquote className="blockquote mb-0">
           <p style={{ textAlign: "justify" }}>
-            Hi Everyone, I am <span className="purple">Soumyajit Behera </span>
-            from <span className="purple"> Bhubaneswar, India.</span>
-            <br />I am a junior pursuing IMSC in Maths and Computing in BIT
-            Mesra.
+            Hi Everyone, I am <span className="purple">Hitenkumar Patel </span>
+            <br />I am a physics major at Brock University
             <br />
             <br />
             Apart from coding, some other activities that I love to do!
@@ -28,10 +26,6 @@ function AboutCard() {
             </li>
           </ul>
 
-          <p style={{ color: "rgb(155 126 172)" }}>
-            "Strive to build things that make a difference!"{" "}
-          </p>
-          <footer className="blockquote-footer">Soumyajit</footer>
         </blockquote>
       </Card.Body>
     </Card>
