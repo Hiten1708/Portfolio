9fb4097b957ef221a4221fa35a3a5aa73af2ad57
Hitenkumar Patel <68720970+Hiten1708@users.noreply.github.com>
2022-08-25 00:53:49 -0400

    Update Home2.js

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -86,7 +86,7 @@ function Home2() {
               </li>
               <li className="social-icons">
                 <a
-                  href="https://www.linkedin.com/in/hitenkumar-patel-79287b1a8/"
+                  href="www.linkedin.com/in/hitenkumar-patel"
                   target="_blank"
                   rel="noreferrer"
                   className="icon-colour  home-social-icons"
