6b3c7023a30b637722ab9ee764cec8bb5598185f
Hiten1708 <68720970+Hiten1708@users.noreply.github.com>
2022-11-29 23:45:31 -0500

    update projects

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -2,12 +2,11 @@ import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import ProjectCard from "./ProjectCards";
 import Particle from "../Particle";
-import leaf from "../../Assets/Projects/leaf.png";
-import emotion from "../../Assets/Projects/emotion.jpeg";
-import editor from "../../Assets/Projects/codeEditor.png";
-import chatify from "../../Assets/Projects/chatify.png";
-import suicide from "../../Assets/Projects/suicide.png";
-import bitsOfCode from "../../Assets/Projects/blog.png";
+import car from "../../Assets/Projects/car.png";
+import website from "../../Assets/Projects/website.png";
+import cafe from "../../Assets/Projects/cafe.png";
+import cancer from "../../Assets/Projects/cancer.png";
+import movie from "../../Assets/Projects/movie.gif";
 
 function Projects() {
   return (
@@ -23,64 +22,54 @@ function Projects() {
         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={chatify}
+              imgPath={cafe}
               isBlog={false}
-              title="Chatify"
-              description="Personal Chat Room or Workspace to share resources and hangout with friends build with react.js, Material-UI, and Firebase. Have features which allows user for realtime messaging, image sharing as well as supports reactions on messages."
-              link="https://github.com/soumyajit4419/Chatify"
+              title="Café API"
+              description="Implemented with Python using Flask and SQLAlchemy and documented with POSTMAN, this API provides users with different information regarding nearby cafes based on users' calls"
+              link="https://github.com/Hiten1708/CAFE_API"
             />
           </Col>
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={bitsOfCode}
+              imgPath={movie}
               isBlog={false}
-              title="Bits-0f-C0de"
-              description="My personal blog page build with Next.js and Tailwind Css which takes the content from makdown files and renders it using Next.js. Supports dark mode and easy to write blogs using markdown."
-              link="https://github.com/soumyajit4419/Bits-0f-C0de"
+              title="10 Movies"
+              description="Developed using Flask as Backend and React as Frontend, its a website that allows users to rank movies according to their ratings and generate movie cards based on their input"
+              link="https://github.com/Hiten1708/10MOVIES"
             />
           </Col>
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={editor}
+              imgPath={website}
               isBlog={false}
-              title="Editor.io"
-              description="Online code and markdown editor build with react.js. Online Editor which supports html, css, and js code with instant view of website. Online markdown editor for building README file which supports GFM, Custom Html tags with toolbar and instant preview.Both the editor supports auto save of work using Local Storage"
-              link="https://github.com/soumyajit4419/Editor.io"
+              title="MbrllaHacks Website"
+              description="Implemented and improvised concept/layout of the overall website in React, CSS, Bootstrap and implemented responsive pages then deployed on Vercel which attracted 50% more developers and volunteers"
+              link="https://github.com/MbrllaHacks/Website"
             />
           </Col>
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={leaf}
+              imgPath={car}
               isBlog={false}
-              title="Plant AI"
+              title="Car Classification with KNN"
               description="Used the plant disease dataset from Kaggle and trained a image classifer model using 'PyTorch' framework using CNN and Transfer Learning with 38 classes of various plant leaves. The model was successfully able to detect diseased and healthy leaves of 14 unique plants. I was able to achieve an accuracy of 98% by using Resnet34 pretrained model."
-              link="https://github.com/soumyajit4419/Plant_AI"
+              link="https://colab.research.google.com/drive/1UvBeVbsjnwkyS4K29vGuPV_neid2fWYe?usp=sharing"
             />
           </Col>
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={suicide}
+              imgPath={cancer}
               isBlog={false}
-              title="Ai For Social Good"
-              description="Using 'Natural Launguage Processing' for the detection of suicide-related posts and user's suicide ideation in cyberspace  and thus helping in sucide prevention."
-              link="https://github.com/soumyajit4419/AI_For_Social_Good"
+              title="Breast Cancer Prediction With SVM"
+              description="Constructed machine learning model to predict the diagnosis of a breast tumor by the SVM Classification model using scikit-learn, Pandas, NumPy, Matplotlib and evaluated the model using confusion matrix the receiver operating curves (ROC) with 96% accuracy"
+              link="https://colab.research.google.com/drive/1UvBeVbsjnwkyS4K29vGuPV_neid2fWYe?usp=sharing"
             />
           </Col>
 
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={emotion}
-              isBlog={false}
-              title="Face Recognition and Emotion Detection"
-              description="Trained a CNN classifier using 'FER-2013 dataset' with Keras and tensorflow backened. The classifier sucessfully predicted the various types of emotions of human. And the highest accuracy obtained with the model was 60.1%.
-              Then used Open-CV to detect the face in an image and then pass the face to the classifer to predict the emotion of a person."
-              link="https://github.com/soumyajit4419/Face_And_Emotion_Detection"
-            />
-          </Col>
         </Row>
 
 
